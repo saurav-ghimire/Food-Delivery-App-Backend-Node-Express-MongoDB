2f05fd7d73d8840e65d66d186a43a225dad68a6d
saurav-ghimire <sauravghimire0123@gmail.com>
2024-06-30 11:44:39 -0400

    Add to cart functionality created

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,8 +1,21 @@
-import userModel from "../models/userModel";
+import userModel from "../models/userModel.js";
 
 // add item to user cart
 const addToCart = async (req,res) => {
-
+  try {
+    let userData = await userModel.findOne({_id : req.body.userId});
+    let cartData = await userData.cartData;
+    if(!cartData[req.body.itemId]){
+      cartData[req.body.itemId] = 1
+    }else{
+      cartData[req.body.itemId] += 1
+    }
+    await userModel.findByIdAndUpdate(req.body.userId,{cartData});
+    res.json({success:true, message:"Added to Cart"});
+  } catch (error) {
+    console.log(error);
+    res.json({success:false, message:"Error"});
+  }
 }
 
 
--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { addToCart, getCart, removeFromCart } from "../controllers/cartController";
+import { addToCart, getCart, removeFromCart } from "../controllers/cartController.js";
 import authMiddeware from "../middleware/auth.js";
 
 const cartRouter = express.Router();
 
 
-router.post('/add',authMiddeware, addToCart);
-router.post('/remove', authMiddeware ,removeFromCart);
-router.get('/get', authMiddeware ,getCart);
+cartRouter.post('/add',authMiddeware, addToCart);
+cartRouter.post('/remove', authMiddeware ,removeFromCart);
+cartRouter.get('/get', authMiddeware ,getCart);
 
 export default cartRouter

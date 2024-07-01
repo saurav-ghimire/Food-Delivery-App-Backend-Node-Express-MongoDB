3d92f02d9f49e6df6a92401dc2662bf371d6ca92
saurav-ghimire <sauravghimire0123@gmail.com>
2024-07-01 17:32:13 -0400

    minor url changes

--- admin/app/(pages)/add/page.jsx
+++ admin/app/(pages)/add/page.jsx
@@ -9,7 +9,7 @@ import {toast } from 'react-toastify';
 function Add() {
 
 
-  const url = "http://localhost:4000";
+  const url = process.env.NEXT_PUBLIC_BACKEND_API_URL;
   const [image, setImage] = useState(null);
   const [data, setData] = useState({
     foodname: '',

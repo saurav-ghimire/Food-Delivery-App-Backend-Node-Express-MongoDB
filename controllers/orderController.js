e54af2da1550962d3da286932783072230214e68
saurav-ghimire <sauravghimire0123@gmail.com>
2024-07-06 15:52:51 -0400

    Order Controller created to save the data and stripe is setuped

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,12 +1,59 @@
-import stripe from "stripe";
+import Stripe from "stripe";
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 
+const stripe = new Stripe(process.env.STRIPE_SECRET);
 
 // Placing user Order from frontend
 
 const placeOrder = async (req,res) => {
+    try {
+      const newOrder = new orderModel({
+        userId : req.body.userId,
+        items:req.body.items,
+        amount:req.body.amount,
+        address:req.body.address
+      });
 
+      await newOrder.save();
+      await userModel.findByIdAndUpdate(req.body.userId, {cartData:{}});
+
+      const lineItems = req.body.items.map((item) => ({
+        price_data : {
+          currency : "CAD",
+          product_data : {
+            name:item.name
+          },
+          unit_amount : item.price * 100,
+          quantity : item.quantity
+        }
+
+      }));
+
+      lineItems.push({
+        price_data : {
+          currency : "CAD",
+          product_data : {
+            name:"Delivery Charges"
+          },
+          unit_amount : 2 * 100,
+          quantity : 1
+        }
+      });
+
+      const session = await stripe.checkout.sessions.create({
+        line_items:lineItems,
+        mode:'payment',
+        success_url:`${process.env.FRONTEND_URL}/verify?success=true&orderId=${newOrder._id}`,
+        cancel_url:`${process.env.FRONTEND_URL}/verify?success=false&orderId=${newOrder._id}`,
+      });
+
+      res.json({success:true, success_url:session.url})
+      
+    } catch (error) {
+      console.log(error);
+      res.json({success:false, message:'Error'})
+    }
 }
 
 export {

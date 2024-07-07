914ab3dee367877cc903b31474b9ea3b7ebe8163
saurav-ghimire <sauravghimire0123@gmail.com>
2024-07-07 11:55:02 -0400

    Verify Order after payment

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -63,6 +63,25 @@ const placeOrder = async (req, res) => {
   }
 };
 
+  const verfiyOrder = async(req,res) => {
+
+    const {orderId, success} = req.body;
+    try {
+      if(success=="true") {
+        await orderModel.findByIdAndUpdate(orderId, {payment:true});
+        res.json({success:true, message:"Paid"});
+      }else{
+        await orderModel.findByIdAndDelete(orderId);
+        res.json({success:false, message:"Not Paid"});
+      }
+    } catch (error) {
+      console.log(error)
+      res.json({success:false, message:"Error"});
+    }
+
+  }
+
 export {
-  placeOrder
-};
+  placeOrder,
+  verfiyOrder
+  };
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
 import authMiddeware from "../middleware/auth.js";
-import { placeOrder } from "../controllers/orderController.js";
+import { placeOrder, verfiyOrder } from "../controllers/orderController.js";
 const orderRouter = express.Router();
 
 
 orderRouter.post('/place', authMiddeware, placeOrder);
+orderRouter.post('/verify', authMiddeware, verfiyOrder);
 
 export default orderRouter;

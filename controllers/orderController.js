315ddcc49f92ea29bbfb35aa82db6d4637f4034d
saurav-ghimire <sauravghimire0123@gmail.com>
2024-07-06 22:17:38 -0400

    Stripe Payment is integrated with backend

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,56 +6,63 @@ const stripe = new Stripe(process.env.STRIPE_SECRET);
 
 // Placing user Order from frontend
 
-const placeOrder = async (req,res) => {
-    try {
-      const newOrder = new orderModel({
-        userId : req.body.userId,
-        items:req.body.items,
-        amount:req.body.amount,
-        address:req.body.address
-      });
+const placeOrder = async (req, res) => {
+  try {
+    const newOrder = new orderModel({
+      userId: req.body.userId,
+      items: req.body.items,
+      amount: req.body.amount,
+      address: req.body.address
+    });
 
-      await newOrder.save();
-      await userModel.findByIdAndUpdate(req.body.userId, {cartData:{}});
+    await newOrder.save();
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
 
-      const lineItems = req.body.items.map((item) => ({
-        price_data : {
-          currency : "CAD",
-          product_data : {
-            name:item.name
-          },
-          unit_amount : item.price * 100,
-          quantity : item.quantity
-        }
+    const lineItems = req.body.items.map((item) => ({
+      price_data: {
+        currency: "CAD",
+        product_data: {
+          name: item.name
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity
+    }));
 
-      }));
+    console.log(lineItems);
 
-      lineItems.push({
-        price_data : {
-          currency : "CAD",
-          product_data : {
-            name:"Delivery Charges"
-          },
-          unit_amount : 2 * 100,
-          quantity : 1
-        }
-      });
+    lineItems.push({
+      price_data: {
+        currency: "CAD",
+        product_data: {
+          name: "Delivery Charges"
+        },
+        unit_amount: 2 * 100,
+      },
+      quantity: 1
+    });
 
-      const session = await stripe.checkout.sessions.create({
-        line_items:lineItems,
-        mode:'payment',
-        success_url:`${process.env.FRONTEND_URL}/verify?success=true&orderId=${newOrder._id}`,
-        cancel_url:`${process.env.FRONTEND_URL}/verify?success=false&orderId=${newOrder._id}`,
-      });
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: `${process.env.FRONTEND_URL}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${process.env.FRONTEND_URL}/verify?success=false&orderId=${newOrder._id}`,
+    });
 
-      res.json({success:true, success_url:session.url})
-      
-    } catch (error) {
-      console.log(error);
-      res.json({success:false, message:'Error'})
+    
+    if (!session.url) {
+      throw new Error('Session URL is undefined');
     }
-}
+
+    res.json({ success: true, success_url: session.url });
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: 'Error' });
+  }
+};
 
 export {
   placeOrder
-}
+};
